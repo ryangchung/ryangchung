609f4662f5444511a809f786401feb479738e2e3
Ryan C <ryan.chung.cs@gmail.com>
2025-07-18 14:59:06 -0400

    feat(aerospace): add aerospace to setup

--- install.sh
+++ install.sh
@@ -26,5 +26,12 @@ echo "Linking .zshrc..."
 ln -sfn /Users/ryan/ryangchung/zsh/.zshrc "$HOME/.zshrc"
 echo "Linked ~/.zshrc -> ~/Users/ryan/ryangchung/.zshrc"
 
+# 3. Symlink .aerospace.toml
+echo "Deleting current .aerospace.toml"
+rm ~/.aerospace.toml
+echo "Linking .aerospace.toml..."
+ln -sfn /Users/ryan/ryangchung/aerospace/.aerospace.toml "$HOME/.aerospace.toml"
+echo "Linked ~/.aerospace -> ~/Users/ryan/ryangchung/.aerospace.toml"
+
 # 3. Build nix flake using nix-darwin
 sudo nix --extra-experimental-features nix-command --extra-experimental-features flakes run nix-darwin/master#darwin-rebuild -- switch --flake ~/ryangchung/nix
