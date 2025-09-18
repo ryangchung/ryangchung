c79f05b4d5744dbf60fb536eb9880781c4a0550d
Ryan Chung <ryan.chung.cs@gmail.com>
2025-09-18 18:50:29 -0400

    feat(obsidian): enable obsidian
    Signed-off-by: Ryan Chung <ryan.chung.cs@gmail.com>

--- modules/home/programs/obsidian.nix
+++ modules/home/programs/obsidian.nix
@@ -1,37 +1,37 @@
 {
   programs.obsidian = {
-    enable = false;
+    enable = true;
     defaultSettings = {
       communityPlugins = [
 
       ];
-      corePlugins = [
-        "backlink"
-        "bookmarks"
-        "canvas"
-        "command-palette"
-        "daily-notes"
-        "editor-status"
-        "file-explorer"
-        "file-recovery"
-        "global-search"
-        "graph"
-        "markdown-importer"
-        "note-composer"
-        "outgoing-link"
-        "outline"
-        "page-preview"
-        "properties"
-        "publish"
-        "random-note"
-        "slash-command"
-        "slides"
-        "switcher"
-        "tag-pane"
-        "templates"
-        "word-count"
-        "workspaces"
-      ];
+      # corePlugins = [
+      #   "backlink"
+      #   "bookmarks"
+      #   "canvas"
+      #   "command-palette"
+      #   "daily-notes"
+      #   "editor-status"
+      #   "file-explorer"
+      #   "file-recovery"
+      #   "global-search"
+      #   "graph"
+      #   "markdown-importer"
+      #   "note-composer"
+      #   "outgoing-link"
+      #   "outline"
+      #   "page-preview"
+      #   "properties"
+      #   "publish"
+      #   "random-note"
+      #   "slash-command"
+      #   "slides"
+      #   "switcher"
+      #   "tag-pane"
+      #   "templates"
+      #   "word-count"
+      #   "workspaces"
+      # ];
       app = { };
       appearance = { };
       hotkeys = { };
