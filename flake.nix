828b6a6917cc839e54a280de21e07df8bb94eb68
Ryan Chung <ryan.chung.cs@gmail.com>
2025-12-27 10:58:59 -0500

    refactor(flake): cleanup for comments and order
    Signed-off-by: Ryan Chung <ryan.chung.cs@gmail.com>

--- flake.nix
+++ flake.nix
@@ -2,19 +2,19 @@
   description = "Ryan Chung's distributed NixOS Configurations.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nixos-unified.url = "github:srid/nixos-unified";
     flake-parts.url = "github:hercules-ci/flake-parts";
 
-    home-manager.url = "github:nix-community/home-manager";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-unified.url = "github:srid/nixos-unified";
+
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
 
-    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
 
-    nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+    nix-index-database.url = "github:nix-community/nix-index-database";
   };
 
   outputs = inputs:
